f36ee592af7c23dbd5722ac4655b648ba3453917
Philip Mateescu <github@philipm.us>
2012-09-23 12:11:14 -0500

    really simple version that just shows the estimates. currently just showing daily estimates

--- js/main.js
+++ js/main.js
@@ -25,14 +25,35 @@ var LeaseInfo = {
 	milesPerYear: 12000,
 }
 
-function estimateMilesOn(aDate, leaseInfo) {
+function estimateMilesOn(aDate, leaseInfo, estimatedMiles) {
 	var aDate = aDate.date(),
 	    today = (new Date()).date();
 	
-	var estimate = estimateMiles(LeaseInfo.leaseLength, LeaseInfo.milesPerYear)
-	var days = Math.floor((aDate - LeaseInfo.leaseStart) / ONE_DAY);
+	var estimate = estimatedMiles || estimateMiles(LeaseInfo.leaseLength, LeaseInfo.milesPerYear)
+	var days = Math.floor((aDate - leaseInfo.leaseStart) / ONE_DAY);
 
 	var expected = estimate.perDay * days;
 	return expected;
 }
 
+function getDateRange(aroundDate, leaseInfo) {
+	var center = aroundDate || (new Date()).date(),
+	start = center - (2 * ONE_DAY);
+	if (start < leaseInfo.leaseStart) {
+		start = leaseInfo.leaseStart;
+	}
+	// TODO: also detect lease end.
+
+	var stack = [], estimate = estimateMiles(leaseInfo.leaseStart, leaseInfo.milesPerYear);
+	for(var i = 0; i < 5; i++) {
+		var day = { day: (new Date(start + (i * ONE_DAY))).date() };
+		var diff = aroundDate - day.day;
+		day.type = diff == 0 ? 'current info' 
+			: diff > 0 ? 'past' : 'future';
+		var onDay = estimateMilesOn(day.day, leaseInfo, estimate);
+		day.estimate = onDay;
+		stack.push(day);
+	}
+
+	return stack;
+}
